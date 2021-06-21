ccce6c702784e08d8be6529a8b7e7362b27876ff
giannurwana19 <giannurwana19@gmail.com>
2021-06-21 11:15:16 +0700

    add class activ carousel in js, add animation about and feature section

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,9 @@
-console.log('ok');
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    duration: 1500,
+    once: false,
+  });
+
+  const carousel = document.querySelector('.carousel-item');
+  carousel.classList.add('active');
+});
